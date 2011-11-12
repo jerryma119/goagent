185992758683eec9eb4602c7d96450527b4012bf
Phus Lu <phus.lu@gmail.com>
2011-11-13 02:02:30 +0800

    uploader

--- server/uploader.bat
+++ server/uploader.bat
@@ -1,7 +1,7 @@
 @echo off
 
 findstr your_appid app.yaml && echo Please add your_appid to app.yaml first!!! && pause && exit
-tasklist | findstr "goagent.exe" && (
+cmd.exe /c tasklist | findstr "goagent.exe" && (
     echo find goagent.exe is running, set proxy to 127.0.0.1
     set HTTP_PROXY=http://127.0.0.1:8087
     set HTTPS_PROXY=http://127.0.0.1:8087
