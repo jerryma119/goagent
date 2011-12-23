3eba6654c9c22bb9e97cb8ce498f8d293c916495
Phus Lu <phus.lu@gmail.com>
2011-12-24 02:51:09 +0800

    fetch.go

--- server/fetch/fetch.go
+++ server/fetch/fetch.go
@@ -8,13 +8,14 @@ import (
 	"log"
 	"bytes"
 	"strings"
+	//"strconv"
 	"encoding/hex"
 	"encoding/binary"
 	"compress/zlib"
+	"io/ioutil"
 
-	//"appengine"
-	//"appengine/urlfetch"
-	
+	"appengine"
+	"appengine/urlfetch"
 	"http"
 )
 
@@ -22,26 +23,30 @@ const (
     Version  = "1.7.0"
     Author   = "phus.lu@gmail.com"
     Password = ""
+    
+    FetchMax     = 3
+    FetchMaxSize = 1024*1024
+    Deadline     = 30
 )
 
-func encodeData(h http.Header) []byte {
+func encodeData(dic map[string]string) []byte {
 	w := bytes.NewBufferString("")
-	for k, vs := range h {
-		fmt.Fprintf(w, "%s=%s&", k, hex.EncodeToString([]byte(vs[0])))
+	for k, v := range dic {
+		fmt.Fprintf(w, "%s=%s&", k, hex.EncodeToString([]byte(v)))
 	}
 	return w.Bytes()
 }
 
-func decodeData(r []byte) http.Header{
-    h := make(http.Header)
-	for _, kv := range strings.Split(string(r), "&") {
+func decodeData(qs []byte) map[string]string {
+    m := make(map[string]string)
+	for _, kv := range strings.Split(string(qs), "&") {
 		if kv != "" {
 			pair := strings.Split(kv, "=")
 			value, _ := hex.DecodeString(pair[1])
-			h.Set(pair[0], string(value))
+			m[pair[0]] = string(value)
 		}
 	}
-	return h;
+	return m;
 }
 
 type Webapp struct {
@@ -59,43 +64,93 @@ func (app Webapp) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func (app Webapp) printResponse(status int, headers http.Header, content []byte) {
-	headersBytes := encodeData(headers)
+func (app Webapp) printResponse(status int, header map[string]string, content []byte) {
+	headerBytes := encodeData(header)
 
 	app.response.WriteHeader(status)
 	app.response.Header().Set("Content-Type", "image/gif")    
    
-    if strings.HasPrefix(headers.Get("content-type"), "text/") {
+    if contentType, ok := header["content-type"]; ok && strings.HasPrefix(contentType, "text/") {
     	app.response.Write([]byte("1"))
-    	w, err := zlib.NewWriter(app.response)
-    	defer w.Close()
+    	w, err := zlib.NewWriter(app.response)	
     	if err != nil {
-    		log.Fatalf("Error: %v", err)
+    		log.Fatalf("zlib.NewWriter(app.response) Error: %v", err)
     	}
+    	defer w.Close()
     	binary.Write(w, binary.BigEndian, uint32(status))
-    	binary.Write(w, binary.BigEndian, uint32(len(headersBytes)))
+    	binary.Write(w, binary.BigEndian, uint32(len(headerBytes)))
     	binary.Write(w, binary.BigEndian, uint32(len(content)))
-    	w.Write(headersBytes)
+    	w.Write(headerBytes)
     	w.Write(content)
     } else {
     	app.response.Write([]byte("0"))
     	binary.Write(app.response, binary.BigEndian, uint32(status))
-    	binary.Write(app.response, binary.BigEndian, uint32(len(headersBytes)))
+    	binary.Write(app.response, binary.BigEndian, uint32(len(headerBytes)))
     	binary.Write(app.response, binary.BigEndian, uint32(len(content)))
-    	app.response.Write(headersBytes)
+    	app.response.Write(headerBytes)
     	app.response.Write(content)
     }
 }
 
-func (app Webapp) printNotify(method string, url string, status int, content []byte) {
-    content = []byte("<h2>PHP Fetch Server Info</h2><hr noshade='noshade'><p>$method '$url'</p><p>Return Code: $status</p><p>Message: $content</p>")
-    headers := make(http.Header)
-    headers.Set("Content-Type", "text/html")
+func (app Webapp) printNotify(method string, url string, status int, text string) {
+    content := []byte(fmt.Sprintf("<h2>PHP Fetch Server Info</h2><hr noshade='noshade'><p>%s '%s'</p><p>Return Code: %d</p><p>Message: %s</p>", method, url, status, text))
+    headers := map[string]string {"Content-Type": "text/html"}
     app.printResponse(status, headers, content)
 }
 
 func (app Webapp) post() {
-	app.printNotify("", "", 200, []byte("hello world"))
+	r, err := zlib.NewReader(app.request.Body)
+	if err != nil {
+		log.Fatalf("zlib.NewReader(app.request.Body) Error: %v", err)
+    }
+    defer r.Close()
+    data, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatalf("io.ReadFull(r) Error: %v", err)
+    }    
+    request := decodeData(data)
+     
+    method  := request["method"]
+    url     := request["url"]
+    headers := request["headers"]
+    
+    if Password != "" {
+    	password, ok := request["password"]
+    	if !ok || password != Password {
+    		app.printNotify(method, url, 403, "Wrong Password.")
+    	}
+    }
+    
+    if !strings.HasPrefix(url, "http") {
+    	app.printNotify(method, url, 501, "Unsupported Scheme")
+    }
+    
+    payload := strings.NewReader(request["payload"])
+    req, err := http.NewRequest(method, url, payload)
+    if err != nil {
+    	app.printNotify(method, url, 500, "http.NewRequest(method, url, payload) failed")
+    }
+    
+    for _, line := range strings.Split(headers, "\r\n") {
+    	kv := strings.SplitN(line, ":", 2)
+    	if len(kv) == 2 {
+    		req.Header.Set(strings.TrimSpace(kv[0]), strings.TrimSpace(kv[1]))
+    	}
+    }
+    
+    var errors []string
+    for i := 0; i < FetchMax; i++ {
+    	t := &urlfetch.Transport{Context:appengine.NewContext(app.request), DeadlineSeconds:float64(Deadline)}
+    	resp, err := t.RoundTrip(req)
+    	if err == nil {
+        	//app.printResponse(resp.StatusCode, resp.Header, ioutil.ReadAll(resp.Body));
+        	app.printNotify(method, url, 200, fmt.Sprintf("resp.StatusCode=%v resp.Header=%v resp.ContentLength=%v", resp.StatusCode, resp.Header, resp.ContentLength))
+        	return
+    	} else {
+    		errors = append(errors, err.String())
+    	}
+    }
+	app.printNotify(method, url, 502, fmt.Sprintf("Fetch Server Failed: %v", errors))
 }
 
 func (app Webapp) get() {
