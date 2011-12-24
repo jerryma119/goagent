7b6f486523d2a8525109f0704c9b94c6b100f75f
Phus Lu <phus.lu@gmail.com>
2011-12-24 22:57:21 +0800

    no utf8

--- server/fetch/fetch.go
+++ server/fetch/fetch.go
@@ -219,23 +219,23 @@ func (app Webapp) get() {
 <head>
     <link rel="icon" type="image/vnd.microsoft.icon" href="http://www.google.cn/favicon.ico">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>GoAgent %s 已经在工作了</title>
+    <title>GoAgent GAE/Go %s &#x5DF2;&#x7ECF;&#x5728;&#x5DE5;&#x4F5C;&#x4E86;</title>
 </head>
 <body>
     <table width="800" border="0" align="center">
         <tr><td align="center"><hr></td></tr>
         <tr><td align="center">
-            <b><h1>GoAgent %s 已经在工作了</h1></b>
+            <b><h1>GoAgent GAE/Go %s &#x5DF2;&#x7ECF;&#x5728;&#x5DE5;&#x4F5C;&#x4E86;</h1></b>
         </td></tr>
         <tr><td align="center"><hr></td></tr>
 
         <tr><td align="center">
-            GoAgent是一个开源的HTTP Proxy软件,使用Go/Python编写,运行于Google App Engine平台上.
+            GoAgent&#x662F;&#x4E00;&#x4E2A;&#x5F00;&#x6E90;&#x7684;HTTP Proxy&#x8F6F;&#x4EF6;, &#x4F7F;&#x7528;Go/Python&#x7F16;&#x5199;, &#x8FD0;&#x884C;&#x4E8E;Google App Engine&#x5E73;&#x53F0;&#x4E0A;
         </td></tr>
         <tr><td align="center"><hr></td></tr>
 
         <tr><td align="center">
-            更多相关介绍,请参考<a href="http://code.google.com/p/goagent/">GoAgent项目主页</a>.
+            &#x66F4;&#x591A;&#x76F8;&#x5173;&#x4ECB;&#x7ECD;, &#x8BF7;&#x53C2;&#x8003;<a href="http://code.google.com/p/goagent/">GoAgent&#x9879;&#x76EE;&#x4E3B;&#x9875;</a>.
         </td></tr>
         <tr><td align="center"><hr></td></tr>
 
