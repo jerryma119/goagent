1178afa81ab722194430b37de59639cd009dc6f7
Phus Lu <phus.lu@gmail.com>
2011-12-26 00:34:10 +0800

    new uploader

--- server/uploader.bat
+++ server/uploader.bat
@@ -2,13 +2,18 @@
 
 cd /d "%~dp0"
 
-findstr your_appid app.yaml && echo Please add your_appid to app.yaml first!!! && pause && exit
-cmd.exe /c tasklist | findstr "goagent.exe" && (
-    echo find goagent.exe is running, set proxy to 127.0.0.1
+cmd.exe /c tasklist | findstr "goagent.exe" >NUL && (
+    rem echo find goagent.exe is running, set proxy to 127.0.0.1
     set HTTP_PROXY=http://127.0.0.1:8087
     set HTTPS_PROXY=http://127.0.0.1:8087
 )
-set PYTHONSCRIPT=appcfg.zip
-"..\local\proxy.exe" rollback . && "..\local\proxy.exe" update . && ping -n 5 127.1 >NUL || pause
+
+(
+    cmd.exe /c "set PYTHONSCRIPT=appid=raw_input('APPID:').strip();yaml=__import__('re').sub(r'application:\s*\w*', 'application: '+appid, open('app.yaml', 'rb').read());open('app.yaml', 'wb').write(yaml);print yaml && ..\local\proxy.exe"
+) && (
+    set PYTHONSCRIPT=appcfg.zip && "..\local\proxy.exe" rollback . && "..\local\proxy.exe" update . && ping -n 5 0.0.0.0 >NUL 
+) || (
+    pause
+)
 
 @echo on
