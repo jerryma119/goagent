5de4e7096917be07ed358b6b91f65ef7e57fe568
Phus Lu <phus.lu@gmail.com>
2011-12-28 23:32:15 +0800

    new uploader

--- server/uploader.py
+++ server/uploader.py
@@ -5,7 +5,103 @@ __version__ = '1.0'
 __author__ = "phus.lu@gmail.com"
 
 import sys, os, re, time
+import socket
+import random
 sys.path.extend(['.', 'appcfg.zip'])
+
+import fancy_urllib
+
+class MultiplexConnection(object):
+    '''multiplex tcp connection class'''
+
+    retry = 3
+    timeout = 5
+    window = 8
+    window_min = 4
+    window_max = 60
+    window_ack = 0
+
+    def __init__(self, hosts, port):
+        self.socket = None
+        self._sockets = set([])
+        self.connect(hosts, port, MultiplexConnection.timeout, MultiplexConnection.window)
+    def connect(self, hostlist, port, timeout, window):
+        for i in xrange(MultiplexConnection.retry):
+            hosts = random.sample(hostlist, window) if len(hostlist) > window else hostlist
+            logging.debug('MultiplexConnection try connect hosts=%s, port=%d', hosts, port)
+            socks = []
+            for host in hosts:
+                sock_family = socket.AF_INET6 if ':' in host else socket.AF_INET
+                sock = socket.socket(sock_family, socket.SOCK_STREAM)
+                sock.setblocking(0)
+                #logging.debug('MultiplexConnection connect_ex (%r, %r)', host, port)
+                err = sock.connect_ex((host, port))
+                self._sockets.add(sock)
+                socks.append(sock)
+            (_, outs, _) = select.select([], socks, [], timeout)
+            if outs:
+                self.socket = outs[0]
+                self.socket.setblocking(1)
+                self._sockets.remove(self.socket)
+                if window > MultiplexConnection.window_min:
+                    MultiplexConnection.window_ack += 1
+                    if MultiplexConnection.window_ack > 10:
+                        MultiplexConnection.window = window - 1
+                        MultiplexConnection.window_ack = 0
+                        logging.info('MultiplexConnection CONNECT port=%s OK 10 times, switch new window=%d', port, MultiplexConnection.window)
+                break
+            else:
+                logging.warning('MultiplexConnection Cannot hosts %r:%r, window=%d', hosts, port, window)
+        else:
+            MultiplexConnection.window = min(int(round(window*1.5)), len(hostlist), self.window_max)
+            MultiplexConnection.window_ack = 0
+            raise RuntimeError(r'MultiplexConnection Connect hosts %s:%s fail %d times!' % (hosts, port, MultiplexConnection.retry))
+    def close(self):
+        for sock in self._sockets:
+            try:
+                sock.close()
+                del sock
+            except:
+                pass
+        del self._sockets
+
+class common(object):
+    GOOGLE_APPSPOT = '203.208.46.1|203.208.46.2|203.208.46.3|203.208.46.4|203.208.46.5|203.208.46.6|203.208.46.7|203.208.46.8'.split('|')
+    HOSTS = {}
+
+def socket_create_connection((host, port), timeout=None, source_address=None):
+    logging.debug('socket_create_connection connect (%r, %r)', host, port)
+    if '.google.com' in host:
+        msg = 'socket_create_connection returns an empty list'
+        try:
+            conn = MultiplexConnection(common.GOOGLE_APPSPOT, port)
+            sock = conn.socket
+            sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
+            return sock
+        except socket.error, msg:
+            logging.error('socket_create_connection connect fail: (%r, %r)', common.GOOGLE_APPSPOT, port)
+            sock = None
+        if not sock:
+            raise socket.error, msg
+    else:
+        msg = 'getaddrinfo returns an empty list'
+        host = common.HOSTS.get(host) or host
+        for res in socket.getaddrinfo(host, port, 0, socket.SOCK_STREAM):
+            af, socktype, proto, canonname, sa = res
+            sock = None
+            try:
+                sock = socket.socket(af, socktype, proto)
+                if isinstance(timeout, (int, float)):
+                    sock.settimeout(timeout)
+                if source_address is not None:
+                    sock.bind(source_address)
+                sock.connect(sa)
+                return sock
+            except socket.error, msg:
+                if sock is not None:
+                    sock.close()
+        raise socket.error, msg
+fancy_urllib._create_connection = socket_create_connection
 
 def upload(dirname, appid):
     assert isinstance(dirname, basestring) and isinstance(appid, basestring)
