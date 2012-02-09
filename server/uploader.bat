bcfbc531191d0c2827c544fe2af9e261de61caa9
Phus Lu <phus.lu@gmail.com>
2012-02-10 01:59:42 +0800

    uploader.bat

--- server/uploader.bat
+++ server/uploader.bat
@@ -5,7 +5,7 @@ set uploaddir=python
 ( 
     echo ===============================================================
     echo  GoAgent服务端部署程序, 开始上传%uploaddir%服务端
-    echo  如果需要上传python服务端, 请修改本文件的uploaddir的值为python
+    echo  如果需要上传golang服务端, 请修改本文件的uploaddir的值为golang
     echo ===============================================================
     echo.
     echo 请输入您的appid, 多个appid请用^|号隔开
@@ -15,9 +15,12 @@ set uploaddir=python
     set PYTHONSCRIPT="import sys;sys.path.insert(0, 'uploader.zip');import appcfg;appcfg.main()"
 ) && (
     "..\local\proxy.exe"
-) || (
-    pause
+) && (
+    echo 上传成功，请编辑proxy.ini把你的appid填进去，谢谢。
 )
+
+@pause>NUL
+
   
   
 @echo off
