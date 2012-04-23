7d84d56a2a51fb8ad051ab0a4032c088c475f48a
Phus Lu <phus.lu@gmail.com>
2012-04-24 00:15:17 +0800

    typo

--- local/proxy.py
+++ local/proxy.py
@@ -522,7 +522,7 @@ class CertUtil(object):
             if not OpenSSL:
                 logging.critical('CA.crt is not exist and OpenSSL is disabled, ABORT!')
                 sys.exit(-1)
-            if os.name == 'nt'
+            if os.name == 'nt':
                 os.system('certmgr.exe -del -n "GoAgent CA" -c -s -r localMachine Root')
             [os.remove(os.path.join('certs', x)) for x in os.listdir('certs')]
             key, crt = CertUtil.makeCA()
