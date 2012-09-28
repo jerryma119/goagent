a75b800d0cf827f2faf2c51cb3a222deb1630e42
Phus Lu <phus.lu@gmail.com>
2012-09-28 20:27:35 +0800

    rename hexieshe to HustMoon

--- local/proxy.py
+++ local/proxy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding:utf-8
 # Based on GAppProxy 2.0.0 by Du XiaoGang <dugang@188.com>
-# Based on WallProxy 0.4.0 by hexieshe <www.ehust@gmail.com>
+# Based on WallProxy 0.4.0 by Hust Moon <www.ehust@gmail.com>
 # Contributor:
 #      Phus Lu        <phus.lu@gmail.com>
 #      Hewig Xu       <hewigovens@gmail.com>
