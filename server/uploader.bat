e71650a26ab237e411d8e0a099c3c9681444874b
Phus Lu <phus.lu@gmail.com>
2012-10-03 11:16:09 +0800

    refine uploader.bat

--- server/uploader.bat
+++ server/uploader.bat
@@ -11,12 +11,14 @@ set uploaddir=python
 ) && (
     @cd /d "%~dp0" 
 ) && (
+    if exist ".appcfg_cookies" (@del /f /q .appcfg_cookies)
+) && (
     set PYTHONSCRIPT="import sys;sys.path.insert(0, 'uploader.zip');import appcfg;appcfg.main()"
 ) && (
     "..\local\proxy.exe"
 ) && (
     echo.
-    echo 上传成功，请编辑proxy.ini把你的appid填进去，谢谢。请按任意键退出程序。
+    echo 上传成功，请忽略Exception KeyError错误, 编辑proxy.ini把你的appid填进去，谢谢。请按任意键退出程序。
 )
 
 @pause>NUL
