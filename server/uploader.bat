d260483c8d9d6ebbc0dc59a64a42265f8ce7f5df
Phus Lu <phus.lu@gmail.com>
2013-01-06 18:31:20 +0800

    support zipfile to python27.exe

--- server/uploader.bat
+++ server/uploader.bat
@@ -13,7 +13,7 @@ set uploaddir=python
 ) && (
     if exist ".appcfg_cookies" (@del /f /q .appcfg_cookies)
 ) && (
-    "..\local\python27.exe" -c "import sys;sys.path.insert(0, 'uploader.zip');import appcfg;appcfg.main()"
+    "..\local\python27.exe" uploader.zip
 ) && (
     echo.
     echo 上传成功，请不要忘记编辑proxy.ini把你的appid填进去，谢谢。按任意键退出程序。
