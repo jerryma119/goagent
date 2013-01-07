3c72890e8c2ae89c83c36d2cd008e0bc961bc78e
Phus Lu <phus.lu@gmail.com>
2013-01-07 17:53:49 +0800

    show proxy.bat window if start failed.

--- local/proxy.bat
+++ local/proxy.bat
@@ -1,1 +1,1 @@
-@start "GoAgent" "%~dp0python27.exe" -c "import sys,os,traceback;__file__='proxy.py';exec '''try:execfile('proxy.py')\nexcept:traceback.print_exc(file=sys.stderr);os.system('pause')'''"
+@start "GoAgent" "%~dp0python27.exe" -c "import sys,os,traceback,ctypes;__file__='proxy.py';exec '''try:execfile('proxy.py')\nexcept:ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 1);traceback.print_exc(file=sys.stderr);os.system('pause')'''"
