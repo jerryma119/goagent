5198b829aa355cbdd3102b8890627de94003a8c0
Phus Lu <phus.lu@gmail.com>
2013-01-07 17:41:28 +0800

    add proxy.bat

--- local/proxy.bat
+++ local/proxy.bat
@@ -1,1 +1,1 @@
-@start "GoAgent" "%~dp0python27.exe" "%~dpn0.py"
+@start "GoAgent" "%~dp0python27.exe" -c "import sys,os,traceback;__file__='proxy.py';exec '''try:execfile('proxy.py')\nexcept:traceback.print_exc(file=sys.stderr);os.system('pause')'''"
