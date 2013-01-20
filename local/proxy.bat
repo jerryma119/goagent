cdaef8e5b72fa754150eb3d53b8efda7b3be2261
Phus Lu <phus.lu@gmail.com>
2013-01-21 01:49:21 +0800

    support GEVENT_RESOLVER=ares in goagent.exe/proxy.bat

--- local/proxy.bat
+++ local/proxy.bat
@@ -1,1 +1,7 @@
-@set GEVENT_MONKEY_PATCH=1 && @start "GoAgent" "%~dp0python27.exe" -c "import sys,os,traceback,ctypes;__file__='proxy.py';exec '''try:execfile('proxy.py')\nexcept:ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 1);traceback.print_exc(file=sys.stderr);os.system('pause')'''"
+(
+	@set GEVENT_RESOLVER=ares
+) && (
+	@set GEVENT_MONKEY_PATCH=1
+) && (
+	@start "GoAgent" "%~dp0python27.exe" -c "import sys,os,traceback,ctypes;__file__='proxy.py';exec '''try:execfile('proxy.py')\nexcept:ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 1);traceback.print_exc(file=sys.stderr);os.system('pause')'''"
+)
