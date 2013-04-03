b43018e7b8cfc39e976f5d55a973598354412ffa
Phus Lu <phus.lu@gmail.com>
2013-04-03 23:42:39 +0800

    update author email

--- local/proxy.py
+++ local/proxy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding:utf-8
-# Based on GAppProxy 2.0.0 by Du XiaoGang <dugang@188.com>
+# Based on GAppProxy 2.0.0 by Du XiaoGang <dugang.2008@gmail.com>
 # Based on WallProxy 0.4.0 by Hust Moon <www.ehust@gmail.com>
 # Contributor:
 #      Phus Lu        <phus.lu@gmail.com>
