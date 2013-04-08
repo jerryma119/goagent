b12add466fb89dd0c669899c47615ebb88bb90e3
Phus Lu <phus.lu@gmail.com>
2013-04-09 02:04:49 +0800

    refine proxy.bat

--- local/proxy.bat
+++ local/proxy.bat
@@ -1,2 +1,19 @@
-@cd /d "%~dp0"
-@start "GoAgent" python27.exe -c "import sys,os,traceback,ctypes;__file__='proxy.py';exec '''try:execfile('proxy.py')\nexcept:ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 1);traceback.print_exc(file=sys.stderr);os.system('pause')'''"
+@start "GoAgent" "%~dp0python27.exe" -B -x "%~dpnx0" && exit /b 0 || pause
+import sys
+import os
+import traceback
+import ctypes
+
+def main():
+    global __file__
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    try:
+        __file__ = 'proxy.py'
+        __import__('proxy').main()
+    except:
+        ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 1);
+        traceback.print_exc(file=sys.stderr);
+        os.system('pause')
+
+if __name__ == '__main__':
+    main()
