3d04911946f6f7f69863ab1313c9ebdfce271488
Phus Lu <phus.lu@gmail.com>
2013-04-09 21:15:00 +0800

    refine server side

--- server/python/wsgi.py
+++ server/python/wsgi.py
@@ -16,8 +16,11 @@ import zlib
 import logging
 import httplib
 import urlparse
-import cStringIO
 import errno
+try:
+    from io import BytesIO
+except ImportError:
+    from cStringIO import StringIO as BytesIO
 try:
     from google.appengine.api import urlfetch
     from google.appengine.runtime import apiproxy_errors
@@ -202,7 +205,7 @@ def gae_application(environ, start_response):
         elif 'gzip' in accept_encoding:
             response_headers['Content-Encoding'] = 'gzip'
             compressobj = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, 0)
-            dataio = cStringIO.StringIO()
+            dataio = BytesIO()
             dataio.write('\x1f\x8b\x08\x00\x00\x00\x00\x00\x02\xff')
             dataio.write(compressobj.compress(data))
             dataio.write(compressobj.flush())
@@ -334,172 +337,6 @@ def forward_socket(local, remote, timeout=60, tick=2, bufsize=8192, maxping=None
             local.close()
         if remote:
             remote.close()
-
-
-class CertUtil(object):
-    """CertUtil module, based on mitmproxy"""
-
-    ca_vendor = 'GoAgent'
-    ca_lock = __import__('threading').Lock()
-
-    @staticmethod
-    def create_ca():
-        key = OpenSSL.crypto.PKey()
-        key.generate_key(OpenSSL.crypto.TYPE_RSA, 2048)
-        ca = OpenSSL.crypto.X509()
-        ca.set_serial_number(0)
-        ca.set_version(2)
-        subj = ca.get_subject()
-        subj.countryName = 'CN'
-        subj.stateOrProvinceName = 'Internet'
-        subj.localityName = 'Cernet'
-        subj.organizationName = CertUtil.ca_vendor
-        subj.organizationalUnitName = '%s Root' % CertUtil.ca_vendor
-        subj.commonName = '%s CA' % CertUtil.ca_vendor
-        ca.gmtime_adj_notBefore(0)
-        ca.gmtime_adj_notAfter(24 * 60 * 60 * 3652)
-        ca.set_issuer(ca.get_subject())
-        ca.set_pubkey(key)
-        ca.add_extensions([
-            OpenSSL.crypto.X509Extension(b'basicConstraints', True, b'CA:TRUE'),
-            OpenSSL.crypto.X509Extension(b'nsCertType', True, b'sslCA'),
-            OpenSSL.crypto.X509Extension(b'extendedKeyUsage', True, b'serverAuth,clientAuth,emailProtection,timeStamping,msCodeInd,msCodeCom,msCTLSign,msSGC,msEFS,nsSGC'),
-            OpenSSL.crypto.X509Extension(b'keyUsage', False, b'keyCertSign, cRLSign'),
-            OpenSSL.crypto.X509Extension(b'subjectKeyIdentifier', False, b'hash', subject=ca), ])
-        ca.sign(key, 'sha1')
-        return key, ca
-
-    @staticmethod
-    def dump_ca(keyfile='CA.key', certfile='CA.crt'):
-        key, ca = CertUtil.create_ca()
-        with open(keyfile, 'wb') as fp:
-            fp.write(OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, key))
-        with open(certfile, 'wb') as fp:
-            fp.write(OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, ca))
-
-    @staticmethod
-    def _get_cert(commonname, certdir='certs', ca_keyfile='CA.key', ca_certfile='CA.crt', sans=[]):
-        import hashlib
-        with open(ca_keyfile, 'rb') as fp:
-            key = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, fp.read())
-        with open(ca_certfile, 'rb') as fp:
-            ca = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, fp.read())
-
-        pkey = OpenSSL.crypto.PKey()
-        pkey.generate_key(OpenSSL.crypto.TYPE_RSA, 2048)
-
-        req = OpenSSL.crypto.X509Req()
-        subj = req.get_subject()
-        subj.countryName = 'CN'
-        subj.stateOrProvinceName = 'Internet'
-        subj.localityName = 'Cernet'
-        subj.organizationalUnitName = '%s Branch' % CertUtil.ca_vendor
-        if commonname[0] == '.':
-            subj.commonName = '*' + commonname
-            subj.organizationName = '*' + commonname
-            sans = ['*'+commonname] + [x for x in sans if x != '*'+commonname]
-        else:
-            subj.commonName = commonname
-            subj.organizationName = commonname
-            sans = [commonname] + [x for x in sans if x != commonname]
-        req.add_extensions([OpenSSL.crypto.X509Extension(b'subjectAltName', True, ', '.join('DNS: %s' % x for x in sans))])
-        req.set_pubkey(pkey)
-        req.sign(pkey, 'sha1')
-
-        cert = OpenSSL.crypto.X509()
-        cert.set_version(2)
-        try:
-            cert.set_serial_number(int(hashlib.md5(commonname).hexdigest(), 16))
-        except OpenSSL.SSL.Error:
-            cert.set_serial_number(int(time.time()*1000))
-        cert.gmtime_adj_notBefore(0)
-        cert.gmtime_adj_notAfter(60 * 60 * 24 * 3652)
-        cert.set_issuer(ca.get_subject())
-        cert.set_subject(req.get_subject())
-        cert.set_pubkey(req.get_pubkey())
-        if commonname[0] == '.':
-            sans = ['*'+commonname] + [s for s in sans if s != '*'+commonname]
-        else:
-            sans = [commonname] + [s for s in sans if s != commonname]
-        cert.add_extensions([OpenSSL.crypto.X509Extension(b'subjectAltName', True, ', '.join('DNS: %s' % x for x in sans))])
-        cert.sign(key, 'sha1')
-
-        keyfile = os.path.join(certdir, commonname + '.key')
-        with open(keyfile, 'wb') as fp:
-            fp.write(OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, pkey))
-        certfile = os.path.join(certdir, commonname + '.crt')
-        with open(certfile, 'wb') as fp:
-            fp.write(OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, cert))
-
-        return keyfile, certfile
-
-    @staticmethod
-    def get_cert(commonname, certdir='certs', ca_keyfile='CA.key', ca_certfile='CA.crt', sans=[]):
-        if len(commonname) >= 32 and commonname.count('.') >= 2:
-            commonname = re.sub(r'^[^\.]+', '', commonname)
-        keyfile = os.path.join(certdir, commonname + '.key')
-        certfile = os.path.join(certdir, commonname + '.crt')
-        if os.path.exists(certfile):
-            return keyfile, certfile
-        elif OpenSSL is None:
-            return ca_keyfile, ca_certfile
-        else:
-            with CertUtil.ca_lock:
-                if os.path.exists(certfile):
-                    return keyfile, certfile
-                return CertUtil._get_cert(commonname, certdir, ca_keyfile, ca_certfile, sans)
-
-    @staticmethod
-    def import_ca(certfile):
-        dirname, basename = os.path.split(certfile)
-        commonname = os.path.splitext(certfile)[0]
-        if OpenSSL:
-            try:
-                with open(certfile, 'rb') as fp:
-                    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, fp.read())
-                    commonname = (v for k, v in x509.get_subject().get_components() if k == 'O').next()
-            except Exception as e:
-                logging.error('load_certificate(certfile=%r) failed:%s', certfile, e)
-        cmd = ''
-        if sys.platform.startswith('win'):
-            cmd = 'cd /d "%s" && .\certmgr.exe -add %s -c -s -r localMachine Root >NUL' % (dirname, basename)
-        elif sys.platform == 'darwin':
-            cmd = 'security find-certificate -a -c "%s" | grep "%s" >/dev/null || security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" "%s"' % (commonname, commonname, certfile)
-        elif sys.platform.startswith('linux'):
-            import platform
-            platform_distname = platform.dist()[0]
-            if platform_distname == 'Ubuntu':
-                pemfile = "/etc/ssl/certs/%s.pem" % commonname
-                new_certfile = "/usr/local/share/ca-certificates/%s.crt" % commonname
-                if not os.path.exists(pemfile):
-                    cmd = 'cp "%s" "%s" && update-ca-certificates' % (certfile, new_certfile)
-        return os.system(cmd)
-
-    @staticmethod
-    def check_ca():
-        #Check CA exists
-        import glob
-        capath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'CA.crt')
-        certdir = os.path.join(os.path.dirname(__file__), 'certs')
-        if not os.path.exists(capath):
-            if not OpenSSL:
-                logging.critical('CA.key is not exist and OpenSSL is disabled, ABORT!')
-                sys.exit(-1)
-            if os.name == 'nt':
-                os.system('certmgr.exe -del -n "%s CA" -c -s -r localMachine Root' % CertUtil.ca_vendor)
-            if os.path.exists(certdir):
-                if os.path.isdir(certdir):
-                    any(os.remove(x) for x in (glob.glob(certdir+'/*.crt')+glob.glob(certdir+'/*.key')))
-                else:
-                    os.remove(certdir)
-                os.mkdir(certdir)
-            CertUtil.dump_ca('CA.key', 'CA.crt')
-        #Check CA imported
-        if CertUtil.import_ca(capath) != 0:
-            logging.warning('install root certificate failed, Please run as administrator/root/sudo')
-        #Check Certs Dir
-        if not os.path.exists(certdir):
-            os.makedirs(certdir)
 
 
 def light_handler(sock, address):
