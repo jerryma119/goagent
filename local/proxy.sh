5e7e49495f022390da4a213a76c786d9d5ea9a45
Phus Lu <phus.lu@gmail.com>
2013-11-10 12:28:23 +0800

    refine proxy.sh

--- local/proxy.sh
+++ local/proxy.sh
@@ -1,44 +1,79 @@
-#!/bin/bash
-# Works on Unix-like system, such as OSX, Archlinux, Ubuntu, etc.
-# This shell has no dependency.
-# Usage:
-#   ./proxy.sh
-#   ./proxy.sh stop
-#   ./proxy.sh restart
-# The default argument is `start`.
+#!/bin/sh
+#
+# goagent init script
+#
 
-# Get goagent path.
-base_dir=$(dirname $0)
+### BEGIN INIT INFO
+# Provides:          goagent
+# Required-Start:    $syslog
+# Required-Stop:     $syslog
+# Should-Start:      $local_fs
+# Should-Stop:       $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Monitor for goagent activity
+# Description:       goagent is a gae proxy forked from gappproxy/wallproxy.
+### END INIT INFO
 
-# Goagent absolute path.
-goa_path=$base_dir/proxy.py
+# **NOTE** bash will exit immediately if any command exits with non-zero.
+set -e
 
-# For stopping the porcess.
-pid_path=$base_dir/.pid
+PACKAGE_NAME=goagent
+PACKAGE_DESC="goagent proxy server"
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:${PATH}
+
+# Choose python version.
+py=python2.7
 
 start() {
-	# Execute with nohup.
-	nohup python $goa_path > /dev/null 2>&1 &
-	echo $! > $pid_path
-	echo Started.
+    echo "Starting ${PACKAGE_DESC}: "
+    nohup /usr/bin/env $py proxy.py > /dev/null 2>&1 &
+    echo $! > ${PACKAGE_NAME}.pid
+    echo "${PACKAGE_NAME}."
 }
 
 stop() {
-	kill -9 `cat $pid_path`
-	echo Stopped.
+    echo "Stopping ${PACKAGE_DESC}: "
+    kill -9 `cat ${PACKAGE_NAME}.pid` || true
+    echo "${PACKAGE_NAME}."
 }
 
-case $1 in
-	'' | 'start' )
-		start
-		;;
+restart() {
+    stop
+    sleep 1
+    start
+}
 
-	'stop' )
-		stop
-		;;
+usage() {
+    N=$(basename "$0")
+    echo "Usage: [sudo] $N {start|stop|restart}" >&2
+    exit 1
+}
 
-	'restart' )
-		stop
-		start
-		;;
+if [ "$(id -u)" != "0" ]; then
+    echo "please use sudo to run ${PACKAGE_NAME}"
+    exit 0
+fi
+
+# `readlink -f` won't work on Mac, this hack should work on all systems.
+cd $(dirname $($py -c "import os; print os.path.realpath('$0')"))
+
+case "$1" in
+    # If no arg is given, start the goagent.
+    # If arg `start` is given, also start goagent.
+    '' | start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    #reload)
+    restart | force-reload)
+        restart
+        ;;
+    *)
+        usage
+        ;;
 esac
+
+exit 0
