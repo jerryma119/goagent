532b88690445fe853d9ddda6a4983ce467a43a69
Phus Lu <phus.lu@gmail.com>
2013-12-29 20:14:35 +0800

    refine index.php

--- server/php/index.php
+++ server/php/index.php
@@ -4,7 +4,8 @@
 $__version__  = '3.1.2';
 $__password__ = '123456';
 $__hostsdeny__ = array(); // $__hostsdeny__ = array('.youtube.com', '.youku.com');
-$__content_type__ = 'image/gif';
+$__content_types__ = array('image/gif', 'image/x-png');
+$__need_encrypt__ = true;
 $__use_curl__ = function_exists('curl_version');
 $__timeout__ = 20;
 $__content__ = '';
@@ -81,8 +82,8 @@ function decode_request($data) {
 
 
 function echo_content($content) {
-    global $__password__;
-    if ($__password__) {
+    global $__password__, $__need_encrypt__;
+    if ($__need_encrypt__) {
         echo $content ^ str_repeat($__password__[0], strlen($content));
     } else {
         echo $content;
@@ -91,10 +92,7 @@ function echo_content($content) {
 
 
 function curl_header_function($ch, $header) {
-    global $__content__;
-    if (!$__content__) {
-        header('Content-Type: ' . $GLOBALS['__content_type__']);
-    }
+    global $__content__, $__need_encrypt__, $__content_types__;
     $pos = strpos($header, ':');
     if ($pos == false) {
         $__content__ .= $header;
@@ -102,6 +100,16 @@ function curl_header_function($ch, $header) {
         $key = join('-', array_map('ucfirst', explode('-', substr($header, 0, $pos))));
         if ($key != 'Transfer-Encoding') {
             $__content__ .= $key . substr($header, $pos);
+        }
+    }
+    if (preg_match('@^Content-Type: ?(audio/|image/|video/|application/octet-stream)@i', $headers)) {
+        $__need_encrypt__ = false;
+    }
+    if (!trim($header)) {
+        if ($__need_encrypt__) {
+            header('Content-Type: ' . $__content_types__[0]);
+        } else {
+            header('Content-Type: ' . $__content_types__[1]);
         }
     }
     return strlen($header);
@@ -121,7 +129,7 @@ function curl_write_function($ch, $content) {
 }
 
 
-function post_with_curl()
+function post()
 {
     list($method, $url, $headers, $kwargs, $body) = @decode_request(@file_get_contents('php://input'));
 
@@ -206,99 +214,18 @@ function post_with_curl()
     if ($GLOBALS['__content__']) {
         echo_content($GLOBALS['__content__']);
     } else if ($errno) {
+        if (!headers_sent()) {
+            if ($__need_encrypt__) {
+                header('Content-Type: ' . $__content_types__[0]);
+            } else {
+                header('Content-Type: ' . $__content_types__[1]);
+            }
+        }
         $content = "HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', "PHP Urlfetch Error curl($errno)",  curl_error($ch));
         echo_content($content);
     }
     curl_close($ch);
 }
-
-
-function post()
-{
-    list($method, $url, $headers, $kwargs, $body) = @decode_request(@file_get_contents('php://input'));
-
-    $timeout = $GLOBALS['__timeout__'];
-    $password = $GLOBALS['__password__'];
-    if ($password) {
-        if (!isset($kwargs['password']) || $password != $kwargs['password']) {
-            header("HTTP/1.0 403 Forbidden");
-            echo message_html('403 Forbidden', 'Wrong Password', 'please edit proxy.ini');
-            exit(-1);
-        }
-    }
-
-    $hostsdeny = $GLOBALS['__hostsdeny__'];
-    if ($hostsdeny) {
-        $urlparts = parse_url($url);
-        $host = $urlparts['host'];
-        foreach ($hostsdeny as $pattern) {
-            if (substr($host, strlen($host)-strlen($pattern)) == $pattern) {
-                echo_content("HTTP/1.0 403\r\n\r\n" . message_html('403 Forbidden', "hostsdeny matched($host)",  $url));
-                exit(-1);
-            }
-        }
-    }
-
-    if ($body) {
-        $headers['Content-Length'] = strval(strlen($body));
-    }
-    $headers['Connection'] = 'close';
-
-    $header_data = '';
-    foreach ($headers as $key => $value) {
-        if ($key) {
-            $header_data .= join('-', array_map('ucfirst', explode('-', $key))).': '.$value."\r\n";
-        }
-    }
-
-    $options = array('http' => array(), 'ssl' => array());
-    $options['http']['method'] = $method;
-    $options['http']['header'] = $header_string;
-    if ($body) {
-        $options['http']['content'] = $body;
-    }
-    $options['http']['timeout'] = $timeout;
-    $options['http']['follow_location'] = false;
-    $options['ssl']['ciphers'] = 'ALL:@STRENGTH';
-    if (isset($kwargs['validate']) && intval($kwargs['validate'])) {
-        $options['ssl']['verify_peer'] = false;
-        $options['ssl']['capture_peer_cert'] = false;
-    }
-
-    $context = stream_context_create($options);
-    if ($context == false) {
-        echo_content("HTTP/1.0 502\r\n\r\n" . message_html('502 PHP urlfetch error', 'stream_context_create error', var_export($options, true)));
-        exit(-1);
-    }
-
-    $fp = @fopen($url, 'rb', false, $context);
-
-    $metadata = stream_get_meta_data($fp);
-    if ($metadata == false) {
-        echo_content("HTTP/1.0 502\r\n\r\n" . message_html('502 PHP urlfetch error', 'stream_get_meta_data error', var_export($options, true)));
-        exit(-1);
-    }
-
-    if (isset($metadata['wrapper_data']['headers']) && !$metadata['wrapper_data']['headers'] && $metadata['wrapper_type'] == 'cURL') {
-        echo_content("HTTP/1.0 502\r\n\r\n" . message_html('502 PHP urlfetch error', 'Wrong PHP Runtime, please recompile without –with-curlwrapper', var_export($metadata, true)));
-        exit(-1);
-    }
-
-    // for debug
-    // echo_content("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n");
-
-    echo_content(join("\r\n", $metadata['wrapper_data']) . "\r\n\r\n");
-
-    while (!feof($fp)) {
-        $content = fread($fp, 8192);
-        if (!$content) {
-            break;
-        }
-        echo_content($content);
-    }
-    fclose($fp);
-}
-
 
 function get() {
     $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
@@ -313,11 +240,7 @@ function get() {
 
 function main() {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if ($GLOBALS['__use_curl__']) {
-            post_with_curl();
-        } else {
-            post();
-        }
+        post();
     } else {
         get();
     }
