4158876b8b617d5eec1283738dcdd175c3d03fef
Phus Lu <phus.lu@gmail.com>
2014-01-01 23:33:33 +0800

    enable socket cache for php urlfetch

--- local/proxy.py
+++ local/proxy.py
@@ -2412,7 +2412,8 @@ def php_urlfetch(method, url, headers, payload, fetchserver, **kwargs):
     app_payload = b''.join((struct.pack('!h', len(metadata)), metadata, payload))
     fetchserver += '?%s' % random.random()
     crlf = 0 if fetchserver.startswith('https') else common.PHP_CRLF
-    response = http_util.request('POST', fetchserver, app_payload, {'Content-Length': len(app_payload)}, crlf=crlf)
+    connection_cache_key = urlparse.urlparse(fetchserver).netloc
+    response = http_util.request('POST', fetchserver, app_payload, {'Content-Length': len(app_payload)}, crlf=crlf, connection_cache_key=connection_cache_key)
     if not response:
         raise socket.error(errno.ECONNRESET, 'urlfetch %r return None' % url)
     response.app_status = response.status
