015c72218c5c5dd763e3ed96d87959c5b3d3e14d
Phus Lu <phus.lu@gmail.com>
2014-01-27 13:16:18 +0800

    refine dnsproxy

--- local/dnsproxy.py
+++ local/dnsproxy.py
@@ -90,13 +90,16 @@ class DNSServer(gevent.server.DatagramServer):
     """DNS Proxy based on gevent/dnslib"""
 
     def __init__(self, *args, **kwargs):
-        dns_servers = kwargs.pop('dns_servers')
         dns_blacklist = kwargs.pop('dns_blacklist')
+        dns_internal_servers = kwargs.pop('dns_internal_servers')
+        dns_external_servers = kwargs.pop('dns_external_servers')
         dns_timeout = kwargs.pop('dns_timeout', 2)
         super(self.__class__, self).__init__(*args, **kwargs)
-        self.dns_servers = dns_servers
-        self.dns_v4_servers = [x for x in dns_servers if ':' not in x]
-        self.dns_v6_servers = [x for x in dns_servers if ':' in x]
+        self.dns_internal_servers = dns_internal_servers
+        self.dns_external_servers = dns_external_servers
+        self.dns_servers = dns_internal_servers + dns_external_servers
+        self.dns_v4_servers = [x for x in self.dns_servers if ':' not in x]
+        self.dns_v6_servers = [x for x in self.dns_servers if ':' in x]
         self.dns_blacklist = set(dns_blacklist)
         self.dns_cache = ExpireCache(max_size=65536)
         self.dns_timeout = int(dns_timeout)
@@ -127,26 +130,32 @@ class DNSServer(gevent.server.DatagramServer):
                 for dnsserver in self.dns_v6_servers:
                     sock_v6.sendto(data, (dnsserver, 53))
                 timeout_at = time.time() + self.dns_timeout
-                replied_servers = set()
+                need_reply_servers = set(self.dns_servers)
                 while time.time() < timeout_at:
                     if reply_data:
                         break
                     ins, _, _ = select.select(socks, [], [], 0.1)
                     for sock in ins:
-                        reply_data, reply_addr = sock.recvfrom(512)
+                        reply_data, (reply_server, _) = sock.recvfrom(512)
                         reply = dnslib.DNSRecord.parse(reply_data)
                         iplist = [str(x.rdata) for x in reply.rr]
                         if any(x in self.dns_blacklist for x in iplist):
                             logging.warning('query qname=%r reply bad iplist=%r, continue', qname, iplist)
                             reply_data = ''
                             continue
-                        if reply.header.rcode and len(replied_servers) < len(self.dns_servers):
-                            replied_servers.add(reply_addr[0])
-                            logging.warning('query qname=%r reply nonzero rcode=%r, try wait other dnsservers=%r reply, continue', qname, reply.header.rcode, set(self.dnsservers)-replied_servers)
-                            reply_data = ''
-                            continue
+                        if reply.header.rcode and need_reply_servers and reply_server not in self.dns_external_servers:
+                            try:
+                                need_reply_servers.remove(reply_server)
+                            except KeyError:
+                                pass
+                            if need_reply_servers:
+                                logging.warning('query qname=%r qtype=%r reply nonzero rcode=%r, wait other need_reply_servers=%s, continue', qname, qtype, reply.header.rcode, need_reply_servers)
+                                reply_data = ''
+                                continue
+                            else:
+                                logging.info('query qname=%r qtype=%r reply nonzero rcode=%r', qname, qtype, reply.header.rcode)
                         ttl = max(x.ttl for x in reply.rr) if reply.rr else 600
-                        logging.debug('query qname=%r qtype=%r reply iplist=%s, ttl=%r', qname, qtype, iplist, ttl)
+                        logging.debug('query qname=%r qtype=%r reply_server=%r reply iplist=%s, ttl=%r', qname, qtype, reply_server, iplist, ttl)
                         self.dns_cache.set((qname, qtype), reply_data, ttl*2)
                         break
             except socket.error as e:
@@ -159,10 +168,11 @@ class DNSServer(gevent.server.DatagramServer):
 
 def test():
     logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
-    dns_servers = ['8.8.8.8', '114.114.114.114']
+    dns_internal_servers = ['114.114.114.114', '114.114.115.115']
+    dns_external_servers = ['8.8.8.8', '8.8.4.4']
     dns_blacklist = '1.1.1.1|255.255.255.255|74.125.127.102|74.125.155.102|74.125.39.102|74.125.39.113|209.85.229.138|4.36.66.178|8.7.198.45|37.61.54.158|46.82.174.68|59.24.3.173|64.33.88.161|64.33.99.47|64.66.163.251|65.104.202.252|65.160.219.113|66.45.252.237|72.14.205.104|72.14.205.99|78.16.49.15|93.46.8.89|128.121.126.139|159.106.121.75|169.132.13.103|192.67.198.6|202.106.1.2|202.181.7.85|203.161.230.171|203.98.7.65|207.12.88.98|208.56.31.43|209.145.54.50|209.220.30.174|209.36.73.33|209.85.229.138|211.94.66.147|213.169.251.35|216.221.188.182|216.234.179.13|243.185.187.3|243.185.187.39'.split('|')
     logging.info('serving at port 53...')
-    DNSServer(('', 53), dns_servers=dns_servers, dns_blacklist=dns_blacklist).serve_forever()
+    DNSServer(('', 53), dns_internal_servers=dns_internal_servers, dns_external_servers=dns_external_servers, dns_blacklist=dns_blacklist).serve_forever()
 
 
 if __name__ == '__main__':
