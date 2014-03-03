906a49692f12ef8d0e879ca0ecd54da3c3592393
Phus Lu <phus.lu@gmail.com>
2014-03-03 15:09:00 +0000

    refine dnsproxy

--- local/dnsproxy.py
+++ local/dnsproxy.py
@@ -31,18 +31,23 @@ except ImportError:
     pygeoip = None
 
 
-def win32dns_query_dnsserver_list():
-    import ctypes, ctypes.wintypes, struct, socket
-    DNS_CONFIG_DNS_SERVER_LIST = 6
-    buf = ctypes.create_string_buffer(2048)
-    ctypes.windll.dnsapi.DnsQueryConfig(DNS_CONFIG_DNS_SERVER_LIST, 0, None, None, ctypes.byref(buf), ctypes.byref(ctypes.wintypes.DWORD(len(buf))))
-    ips = struct.unpack('I', buf[0:4])[0]
-    out = []
-    for i in xrange(ips):
-        start = (i+1) * 4
-        out.append(socket.inet_ntoa(buf[start:start+4]))
-    return out
-
+def get_dnsserver_list():
+    if os.name == 'nt':
+        import ctypes, ctypes.wintypes, struct, socket
+        DNS_CONFIG_DNS_SERVER_LIST = 6
+        buf = ctypes.create_string_buffer(2048)
+        ctypes.windll.dnsapi.DnsQueryConfig(DNS_CONFIG_DNS_SERVER_LIST, 0, None, None, ctypes.byref(buf), ctypes.byref(ctypes.wintypes.DWORD(len(buf))))
+        ips = struct.unpack('I', buf[0:4])[0]
+        out = []
+        for i in xrange(ips):
+            start = (i+1) * 4
+            out.append(socket.inet_ntoa(buf[start:start+4]))
+        return out
+    elif os.path.isfile('/etc/resolv.conf'):
+        with open('/etc/resolv.conf', 'rb') as fp:
+            return re.findall(r'(?m)^nameserver\s+(\S+)', fp.read())
+    else:
+        return []
 
 class ExpireCache(object):
     """ A dictionary-like object, supporting expire semantics."""
@@ -209,7 +214,6 @@ class DNSServer(gevent.server.DatagramServer):
 
 def test():
     logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
-    # dns_intranet_servers = win32dns_query_dnsserver_list()
     dns_servers = ['114.114.114.114', '114.114.115.115', '8.8.8.8', '8.8.4.4']
     dns_blacklist = '1.1.1.1|255.255.255.255|74.125.127.102|74.125.155.102|74.125.39.102|74.125.39.113|209.85.229.138|4.36.66.178|8.7.198.45|37.61.54.158|46.82.174.68|59.24.3.173|64.33.88.161|64.33.99.47|64.66.163.251|65.104.202.252|65.160.219.113|66.45.252.237|72.14.205.104|72.14.205.99|78.16.49.15|93.46.8.89|128.121.126.139|159.106.121.75|169.132.13.103|192.67.198.6|202.106.1.2|202.181.7.85|203.161.230.171|203.98.7.65|207.12.88.98|208.56.31.43|209.145.54.50|209.220.30.174|209.36.73.33|209.85.229.138|211.94.66.147|213.169.251.35|216.221.188.182|216.234.179.13|243.185.187.3|243.185.187.39'.split('|')
     logging.info('serving at port 53...')
